4a4e4d65d3c2c319ec746eb8ba32af91172160e7
chenzhanzhan <963076250@qq.com>
2020-01-10 14:07:55 +0800

    创建tabbar组件并配置路由

--- src/utils/register-vant.js
+++ src/utils/register-vant.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
-import { Button, NavBar, Field, CellGroup, Toast, CountDown } from 'vant'
+import { Button, NavBar, Field, CellGroup, Toast, CountDown, Tabbar, TabbarItem } from 'vant'
 Vue.use(Button) // 注册
   .use(NavBar)
   .use(Field)
   .use(CellGroup)
   .use(Toast)
   .use(CountDown)
+  .use(Tabbar)
+  .use(TabbarItem)
---
+++ src/views/tabbar/index.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="tabbar">
+    <!-- 子路由出口 -->
+    <router-view/>
+     <!-- 子路由出口 -->
+     <!-- 下面导航tabbar -->
+     <van-tabbar route>
+       <van-tabbar-item to="/" icon="wap-home-o">首页</van-tabbar-item>
+       <van-tabbar-item to="/question" icon="comment-o">问答</van-tabbar-item>
+       <van-tabbar-item to="/video" icon="video-o">视频</van-tabbar-item>
+       <van-tabbar-item to="/my" icon="contact">我的</van-tabbar-item>
+        </van-tabbar>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
